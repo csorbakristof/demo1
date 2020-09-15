ae6fc2679eae4872af032ee67776d0fe5c59a077
Kristof <kristof@aut.bme.hu>
2020-09-15 13:05:05 +0200

    3 sorban

--- DemoApp1/DemoApp1/Program.cs
+++ DemoApp1/DemoApp1/Program.cs
@@ -8,6 +8,7 @@ namespace DemoApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
